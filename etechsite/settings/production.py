c3691e3a97f801adb388ea79b096bae5c17c3393
emin <emineliyev87@gmail.com>
2022-08-18 13:15:02 +0400

    End commit 2

--- etechsite/settings/production.py
+++ etechsite/settings/production.py
@@ -84,6 +84,7 @@ WSGI_APPLICATION = 'etechsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
